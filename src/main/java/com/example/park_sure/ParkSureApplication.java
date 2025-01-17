ac7dea09b579e917303709a71bcd094129df5e83
Sushant Kumar <sushant.k052@gmail.com>
2025-01-17 05:32:14 +0530

    Initial commit for - Park sure

---
+++ src/main/java/com/example/park_sure/ParkSureApplication.java
@@ -0,0 +1,13 @@
+package com.example.park_sure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkSureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkSureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/park_sure/ParkSureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.park_sure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkSureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
